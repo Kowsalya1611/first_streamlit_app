e1f2003f8b23012f158759f8def2e4d0a3eb0c5b
Kowsalya1611 <124353600+Kowsalya1611@users.noreply.github.com>
2023-02-13 14:41:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,3 +41,5 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+my_cur.execute("insert into fruit_load_list values('from streamlit')");
